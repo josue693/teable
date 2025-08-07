673ae52220eb74bd4135e9dde136fe9a7e57f25d
Boris <boris2code@outlook.com>
2025-08-07 11:18:57 +0800

    feat: revert view event fix

--- apps/nestjs-backend/src/event-emitter/event-emitter.service.ts
+++ apps/nestjs-backend/src/event-emitter/event-emitter.service.ts
@@ -87,6 +87,7 @@ export class EventEmitterService {
     if (!generatedEvents) {
       return;
     }
+
     const observable = from(Array.from(generatedEvents.values()));
 
     observable
@@ -113,6 +114,7 @@ export class EventEmitterService {
 
   private combineEvents(groupedEvents: OpEvent[]): OpEvent {
     if (groupedEvents.length <= 1) return groupedEvents[0];
+
     return groupedEvents.reduce((combinedEvent, event, index) => {
       const mergePropertyName = this.getMergePropertyName(event);
 
@@ -128,11 +130,7 @@ export class EventEmitterService {
 
   private getMergePropertyName(event: OpEvent): string {
     return match(event)
-      .with(
-        P.union({ name: Events.TABLE_VIEW_CREATE }, { name: Events.TABLE_VIEW_UPDATE }),
-        () => 'view'
-      )
-      .with({ name: Events.TABLE_VIEW_DELETE }, () => 'viewId')
+      .with({ name: Events.TABLE_VIEW_CREATE }, () => 'view')
       .with(
         P.union({ name: Events.TABLE_FIELD_CREATE }, { name: Events.TABLE_FIELD_UPDATE }),
         () => 'field'
@@ -192,6 +190,7 @@ export class EventEmitterService {
           opCreateData: rawOp.create?.data,
           ops: rawOp?.op,
         }) as OpEvent;
+
         const event = this.createEvent(docType, opType, {
           ...extendPlainContext,
           ...plainContext,
