9ff7438ea93b9bb52de1c93bd57abb6e21d9842b
Uno <tianyangchen1@outlook.com>
2025-11-05 18:21:02 +0800

    refactor: replace useToast with sonner toast

--- apps/nextjs-app/src/features/app/blocks/share/view/component/grid/GridViewBase.tsx
+++ apps/nextjs-app/src/features/app/blocks/share/view/component/grid/GridViewBase.tsx
@@ -46,7 +46,8 @@ import {
   useTableId,
   useView,
 } from '@teable/sdk/hooks';
-import { Skeleton, useToast } from '@teable/ui-lib/shadcn';
+import { Skeleton } from '@teable/ui-lib/shadcn';
+import { toast } from '@teable/ui-lib/shadcn/ui/sonner';
 import { uniqueId } from 'lodash';
 import { useRouter } from 'next/router';
 import { useCallback, useEffect, useMemo, useRef } from 'react';
@@ -77,7 +78,6 @@ export const GridViewBase = (props: IGridViewProps) => {
   const gridRef = useRef<IGridRef>(null);
   const container = useRef<HTMLDivElement>(null);
   const expandRecordRef = useRef<IExpandRecordContainerRef>(null);
-  const { toast } = useToast();
   const theme = useGridTheme();
   const rowCount = useRowCount();
   const ssrRecords = useSSRRecords();
@@ -230,12 +230,12 @@ export const GridViewBase = (props: IGridViewProps) => {
     async (selection: CombinedSelection) => {
       const allowCopy = view?.shareMeta?.allowCopy;
       if (!allowCopy) {
-        toast({ title: "Sorry, the table's owner has disabled copying" });
+        toast.warning("Sorry, the table's owner has disabled copying");
         return;
       }
       await copy(selection);
     },
-    [copy, view?.shareMeta?.allowCopy, toast]
+    [copy, view?.shareMeta?.allowCopy]
   );
 
   const onColumnStatisticClick = useCallback(
