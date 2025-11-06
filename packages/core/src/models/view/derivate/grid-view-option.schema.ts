924cc0a60cb09b15d19a8c3a8be8578c5297f963
Bieber <artist@teable.io>
2025-11-06 14:52:59 +0800

    chore: openapi -> meta

--- packages/core/src/models/view/derivate/grid-view-option.schema.ts
+++ packages/core/src/models/view/derivate/grid-view-option.schema.ts
@@ -4,20 +4,20 @@ import { RowHeightLevel } from '../constant';
 export const gridViewOptionSchema = z
   .object({
     rowHeight: z
-      .nativeEnum(RowHeightLevel)
+      .enum(RowHeightLevel)
       .optional()
-      .openapi({ description: 'The row height level of row in view' }),
+      .meta({ description: 'The row height level of row in view' }),
     fieldNameDisplayLines: z
       .number()
       .min(1)
       .max(3)
       .optional()
-      .openapi({ description: 'The field name display lines in view' }),
+      .meta({ description: 'The field name display lines in view' }),
     frozenColumnCount: z
       .number()
       .min(0)
       .optional()
-      .openapi({ description: 'The frozen column count in view' }),
+      .meta({ description: 'The frozen column count in view' }),
   })
   .strict();
 
